0fe110d2c40c3a61244aae7ace552052719bb1fa
Kishore kumar <kk78744@gmail.com>
2019-01-15 19:12:44 +0530

    Promises part 1

--- exercises/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ exercises/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -20,7 +20,11 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit() {
     let id = this.route.snapshot.params['id'];
-    this.dish = this.dishService.getDish(id);
+    // this.dish = this.dishService.getDish(id);
+    // To handle Promises
+    this.dishService.getDish(id).then(
+      dish => this.dish = dish
+    )
   }
 
   goBack(): void {
