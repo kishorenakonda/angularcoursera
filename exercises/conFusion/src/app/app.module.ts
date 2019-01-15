9016648ac88362447fd45114477dbe18c2d6c778
Kishore kumar <kk78744@gmail.com>
2019-01-15 16:37:13 +0530

    single page application part 1

--- exercises/conFusion/src/app/app.module.ts
+++ exercises/conFusion/src/app/app.module.ts
@@ -21,7 +21,9 @@ import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
 
+
 import { AppRoutingModule } from './app-routing.module';
+import { PromotionService } from './services/promotion.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -44,7 +46,7 @@ import { AppRoutingModule } from './app-routing.module';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
